fbd776d58ffda2fc2a8d6f1261b71844b0f4f4fd
Audrey Ha <audreyha@gmail.com>
2019-09-04 11:38:47 -0700

    Implemented separate Canvas class for UIViews
    Still need:
    1. Color changes
    2. Eraser
    3. Slider changes

--- StressGames/DrawingGame.swift
+++ StressGames/DrawingGame.swift
@@ -8,6 +8,52 @@
 
 import UIKit
 
+class Canvas : UIView{
+    override func draw(_ rect: CGRect){
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else {return}
+        
+        var colors=[UIColor(rgb: 0xfa310a), UIColor(rgb: 0xff803f), UIColor(rgb: 0xffe23f), UIColor(rgb: 0xc8ff3f), UIColor(rgb: 0x3fd4ff), UIColor(rgb: 0x003f87), UIColor(rgb: 0x400087), UIColor(rgb: 0x000000)]
+        
+        var colorInteger=UserDefaults.standard.integer(forKey: "colorInteger")
+        print("color integer: \(colorInteger)")
+        context.setStrokeColor(colors[colorInteger].cgColor)
+        
+        
+        var penWidth=UserDefaults.standard.integer(forKey: "penWidth")
+        print("pen width: \(penWidth)")
+        context.setLineWidth(CGFloat(penWidth))
+        context.setLineCap(.round)
+        
+        lines.forEach{(line) in
+            for (i, p) in line.enumerated(){
+                if i==0{
+                    context.move(to: p)
+                }else{
+                    context.addLine(to: p)
+                }
+            }
+        }
+        
+        context.strokePath()
+    }
+    
+    var lines=[[CGPoint]]()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point=touches.first?.location(in: self) else {return}
+        
+        guard var lastLine = lines.popLast() else {return}
+        lastLine.append(point)
+        lines.append(lastLine)
+        
+        setNeedsDisplay()
+    }
+}
 class DrawingGame: UIViewController {
 
     @IBOutlet weak var containerDrawingView: UIView!
@@ -29,9 +75,14 @@ class DrawingGame: UIViewController {
     @IBOutlet weak var penSlider: UISlider!
     @IBOutlet weak var eraserSlider: UISlider!
     
+    @IBOutlet weak var undoButton: UIButton!
+    @IBOutlet weak var screenshotButton: UIButton!
+    @IBOutlet weak var clearAllButton: UIButton!
+    
+    
     var usingPen=true
     
-    var canvas=UIImageView()
+    var canvas=Canvas()
     var colors=[UIColor(rgb: 0xfa310a), UIColor(rgb: 0xff803f), UIColor(rgb: 0xffe23f), UIColor(rgb: 0xc8ff3f), UIColor(rgb: 0x3fd4ff), UIColor(rgb: 0x003f87), UIColor(rgb: 0x400087), UIColor(rgb: 0x000000)]
     
     var buttons=[UIButton]()
@@ -40,13 +91,16 @@ class DrawingGame: UIViewController {
     
     var checkedColorImages=[UIImage(named: "redCheck"), UIImage(named: "orangeCheck"), UIImage(named: "yellowCheck"), UIImage(named: "greenCheck"), UIImage(named: "blueCheck"), UIImage(named: "navyCheck"), UIImage(named: "purpleCheck"), UIImage(named: "blackCheck")]
     
-    var color=UIColor.black
-    var brushWidth: CGFloat=10.0
-    var eraserWidth: CGFloat=10.0
-    var opacity: CGFloat=1.0
+    
+//    var color=UIColor.black
+//    var brushWidth: CGFloat=10.0
+//    var eraserWidth: CGFloat=10.0
+//    var opacity: CGFloat=1.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        UserDefaults.standard.set(4, forKey: "colorInteger")
+        UserDefaults.standard.set(10, forKey: "penWidth")
         
         eraserButton.layer.cornerRadius=5
         penButton.layer.cornerRadius=5
@@ -66,14 +120,19 @@ class DrawingGame: UIViewController {
         
         penSlider.setThumbImage(UIImage(named: "bluePlayBar"), for: .normal)
         eraserSlider.setThumbImage(UIImage(named: "bluePlayBar"), for: .normal)
+        
+        screenshotButton.layer.cornerRadius=10
+        clearAllButton.layer.cornerRadius=10
+        new.layer.cornerRadius=10
+
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
-        canvas.backgroundColor = .white
+        canvas.backgroundColor = .red
         canvas.frame=containerDrawingView.frame
         
-        let gesture = UITapGestureRecognizer(target: self, action:  #selector(checkAction))
-        containerDrawingView.addGestureRecognizer(gesture)
+//        let gesture = UITapGestureRecognizer(target: self, action:  #selector(checkAction))
+//        containerDrawingView.addGestureRecognizer(gesture)
         
         view.addSubview(canvas)
         print("added subview")
@@ -87,68 +146,35 @@ class DrawingGame: UIViewController {
         lines.append([CGPoint]())
     }
     
-    func drawLine(fromPoint: CGPoint, toPoint: CGPoint){
-        UIGraphicsBeginImageContext(canvas.frame.size)
-        guard let context=UIGraphicsGetCurrentContext() else{
-            return
-        }
-        
-        canvas.image?.draw(in:canvas.bounds)
-        
-        context.move(to: fromPoint)
-        context.addLine(to: toPoint)
-        
-        context.setLineCap(.round)
-        context.setBlendMode(.normal)
-        
-        if usingPen==true{
-            context.setLineWidth(brushWidth)
-            context.setStrokeColor(color.cgColor)
-        }else{
-            context.setLineWidth(eraserWidth)
-            context.setStrokeColor(UIColor.white.cgColor)
-        }
-        
-        
-        context.strokePath()
-        canvas.image=UIGraphicsGetImageFromCurrentImageContext()
-        canvas.alpha=opacity
-        UIGraphicsEndImageContext()
-    }
-    
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let point=touches.first?.location(in: canvas) else {return}
 
         guard var lastLine = lines.popLast() else {return}
         lastLine.append(point)
         lines.append(lastLine)
-        
-        var totalCount=lastLine.count
-        
-        if lastLine.count>1{
-            drawLine(fromPoint: lastLine[totalCount-2], toPoint: lastLine[totalCount-1])
-        }
+
         
         //need to redraw line(s) as the line array gets points appended to it
         canvas.setNeedsDisplay()
     }
     
-    @objc func checkAction(sender : UITapGestureRecognizer) {
-        print("tapped")
-        
-        var location=sender.location(in: canvas)
-        var offsetLocation=CGPoint(x: location.x+1, y: location.y+1)
-        drawLine(fromPoint: location, toPoint: offsetLocation)
-        
-        guard var lastLine = lines.popLast() else {return}
-        lastLine.append(location)
-        lastLine.append(offsetLocation)
-        lines.append(lastLine)
-    }
+//    @objc func checkAction(sender : UITapGestureRecognizer) {
+//        print("tapped")
+//
+//        var location=sender.location(in: canvas)
+//        var offsetLocation=CGPoint(x: location.x+1, y: location.y+1)
+//        drawLine(fromPoint: location, toPoint: offsetLocation)
+//
+//        guard var lastLine = lines.popLast() else {return}
+//        lastLine.append(location)
+//        lastLine.append(offsetLocation)
+//        lines.append(lastLine)
+//    }
     
     func setColor(indexPath: Int){
-        color=colors[indexPath]
-    
+        UserDefaults.standard.set(indexPath, forKey: "colorInteger")
+        
         for i in 0...7{
             if i != indexPath{
                 buttons[i].setImage(plainColorImages[i], for: .normal)
@@ -180,11 +206,13 @@ class DrawingGame: UIViewController {
     }
     
     @IBAction func penSliderValueChanged(_ sender: Any) {
-        brushWidth=CGFloat(penSlider.value)
+        //COMMENT: Need to set brush width in user defaults
+//        brushWidth=CGFloat(penSlider.value)
     }
     
     @IBAction func eraserSliderValueChanged(_ sender: Any) {
-        eraserWidth=CGFloat(eraserSlider.value)
+        //COMMENT: set eraser width in user defaults
+//        eraserWidth=CGFloat(eraserSlider.value)
     }
     
     @IBAction func redPressed(_ sender: Any) {
@@ -220,5 +248,18 @@ class DrawingGame: UIViewController {
         setColor(indexPath: 7)
     }
     
+    @IBAction func screenshotPressed(_ sender: Any) {
+    }
     
+    @IBAction func clearAllPressed(_ sender: Any) {
+        print("clear all pressed")
+        lines.removeAll()
+
+    }
+    
+    @IBAction func undoPressed(_ sender: Any) {
+        print("undo pressed")
+        lines.popLast()
+        
+    }
 }
